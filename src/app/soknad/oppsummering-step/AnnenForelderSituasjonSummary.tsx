74ea7157b66b5ab685d9db7632390acfdd60070a
Frode Hansen <frode.hansen@bekk.no>
2020-12-14 10:36:50 +0100

    Remove strong tag

--- src/app/soknad/oppsummering-step/AnnenForelderSituasjonSummary.tsx
+++ src/app/soknad/oppsummering-step/AnnenForelderSituasjonSummary.tsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { FormattedMessage, useIntl } from 'react-intl';
+import bemUtils from '@navikt/sif-common-core/lib/utils/bemUtils';
+import { apiStringDateToDate, prettifyDateExtended } from '@navikt/sif-common-core/lib/utils/dateUtils';
 import intlHelper from '@navikt/sif-common-core/lib/utils/intlUtils';
+import JaNeiSvar from '@navikt/sif-common-soknad/lib/soknad-summary/JaNeiSvar';
 import SummaryBlock from '@navikt/sif-common-soknad/lib/soknad-summary/summary-block/SummaryBlock';
 import SummarySection from '@navikt/sif-common-soknad/lib/soknad-summary/summary-section/SummarySection';
-import { AnnenForelder } from '../../types/SoknadApiData';
-import JaNeiSvar from '@navikt/sif-common-soknad/lib/soknad-summary/JaNeiSvar';
-import Box from '@navikt/sif-common-core/lib/components/box/Box';
 import TextareaSvar from '@navikt/sif-common-soknad/lib/soknad-summary/TextareaSvar';
-import { apiStringDateToDate, prettifyDateExtended } from '@navikt/sif-common-core/lib/utils/dateUtils';
-import FormattedHtmlMessage from '@navikt/sif-common-core/lib/components/formatted-html-message/FormattedHtmlMessage';
-import bemUtils from '@navikt/sif-common-core/lib/utils/bemUtils';
+import { AnnenForelder } from '../../types/SoknadApiData';
 
 const bem = bemUtils('utenlandsoppholdSummaryItem');
 interface Props {
@@ -30,10 +28,9 @@ const AnnenForelderSituasjonSummary = ({ annenForelder }: Props) => {
 
     return (
         <SummarySection header={intlHelper(intl, 'step.oppsummering.annenForelderensSituasjon.header')}>
-            <Box margin="l">
-                <FormattedHtmlMessage id="step.oppsummering.annenForelderensSituasjon.html" />{' '}
+            <SummaryBlock header={intlHelper(intl, 'step.oppsummering.annenForelderensSituasjon.tittel')}>
                 <FormattedMessage id={`grunn.${annenForelder.situasjon}`} />
-            </Box>
+            </SummaryBlock>
 
             {annenForelder.situasjonBeskrivelse && (
                 <SummaryBlock header={intlHelper(intl, 'step.oppsummering.annenForelderensSituasjon.beskrivelse')}>
