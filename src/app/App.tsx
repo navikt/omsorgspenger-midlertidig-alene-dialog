67c00276b00e5e753f73041e49f124127219b202
Frode Hansen <frode.hansen@bekk.no>
2022-12-05 15:29:24 +0100

    Fjerne intro, oppdatere velkommen

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import { AmplitudeProvider } from '@navikt/sif-common-amplitude/lib';
 import { getEnvironmentVariable } from '@navikt/sif-common-core/lib/utils/envUtils';
 import SoknadApplication from '@navikt/sif-common-soknad/lib/soknad-application-setup/SoknadApplication';
 import SoknadApplicationCommonRoutes from '@navikt/sif-common-soknad/lib/soknad-application-setup/SoknadApplicationCommonRoutes';
 import Modal from 'nav-frontend-modal';
 import { applicationIntlMessages } from './i18n/applicationMessages';
-import IntroPage from './pages/intro-page/IntroPage';
 import SoknadRemoteDataFetcher from './soknad/SoknadRemoteDataFetcher';
 import '@navikt/sif-common-core/lib/styles/globalStyles.less';
 import './styles/app.less';
@@ -35,7 +34,9 @@ render(
             publicPath={publicPath}>
             <SoknadApplicationCommonRoutes
                 contentRoutes={[
-                    <Route path="/" key="intro" exact={true} component={IntroPage} />,
+                    <Route path="/" key="redirect" exact={true}>
+                        <Redirect to="/soknad" />
+                    </Route>,
                     <Route path="/soknad" key="soknad" component={SoknadRemoteDataFetcher} />,
                 ]}
             />
--- src/app/soknad/velkommen-page/VelkommenPage.tsx
+++ src/app/soknad/velkommen-page/VelkommenPage.tsx
@@ -1,34 +1,25 @@
 import React from 'react';
 import { useIntl } from 'react-intl';
+import { SIFCommonPageKey, useLogSidevisning } from '@navikt/sif-common-amplitude/lib';
 import Box from '@navikt/sif-common-core/lib/components/box/Box';
-import FrontPageBanner from '@navikt/sif-common-core/lib/components/front-page-banner/FrontPageBanner';
 import Page from '@navikt/sif-common-core/lib/components/page/Page';
 import intlHelper from '@navikt/sif-common-core/lib/utils/intlUtils';
 import { useSoknadContext } from '../SoknadContext';
+import OmSøknaden from './components/OmSøknaden';
+import VelkommenGuide from './components/VelkommenGuide';
 import VelkommenPageForm from './VelkommenPageForm';
-import { Sidetittel } from 'nav-frontend-typografi';
-import { SIFCommonPageKey, useLogSidevisning } from '@navikt/sif-common-amplitude/lib';
+import { Person } from '../../types/Person';
 
-const VelkommenPage = () => {
+const VelkommenPage = ({ søker }: { søker: Person }) => {
     const intl = useIntl();
     const { startSoknad } = useSoknadContext();
     useLogSidevisning(SIFCommonPageKey.velkommen);
 
     return (
-        <Page
-            title={intlHelper(intl, 'application.title')}
-            topContentRenderer={() => (
-                <FrontPageBanner
-                    bannerSize="large"
-                    counsellorWithSpeechBubbleProps={{
-                        strongText: intlHelper(intl, 'step.velkommen.banner.tittel'),
-                        normalText: intlHelper(intl, 'step.velkommen.banner.tekst'),
-                    }}
-                />
-            )}>
-            <Box margin="xxxl" textAlignCenter={true}>
-                <Sidetittel>{intlHelper(intl, 'step.velkommen.tittel')}</Sidetittel>
-            </Box>
+        <Page title={intlHelper(intl, 'application.title')}>
+            <VelkommenGuide navn={søker.fornavn} />
+
+            <OmSøknaden />
 
             <Box margin="xxxl">
                 <VelkommenPageForm onStart={startSoknad} />
